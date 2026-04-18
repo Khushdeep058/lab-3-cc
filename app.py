a3a90d2ebe8f1a32cc18d122aa3878b63d1c989d
Khushdeep058 <160999398+Khushdeep058@users.noreply.github.com>
2026-04-18 20:24:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, bachoooooooooooooooooo!"
+    return "Hello,chudailllllllllllllllllllllllllllllllllllllll kuch bolllllllllllllllllllll yeh wala practical ho gyaaaaaaaaaaaa!"
 
 if __name__ == "__main__":
     app.run()
